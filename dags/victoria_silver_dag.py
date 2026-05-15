d85d617385ec7fab6138eda395e9a0a3ffdfd1bd
Pkpopor <pkpopor@gmail.com>
2026-05-15 13:28:29 +0700

    add silver dag and script

---
+++ dags/victoria_silver_dag.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from scripts.spark.clean_energy import clean_to_silver
+
+default_args = {
+    'retries': 1
+}
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2015, 1, 1),
+    catchup=False
+)
+def victoria_silver_dag():
+
+    @task
+    def run_clean_to_silver(**kwargs):
+        import os
+        bronze_path = kwargs.get('dag_run').conf.get('bronze_path')
+        if not bronze_path:
+            bronze_dir = '/opt/airflow/datalake/bronze/'
+            files = sorted(
+                [f for f in os.listdir(bronze_dir) if f.endswith('.csv')],
+                key=lambda f: os.path.getmtime(os.path.join(bronze_dir, f)),
+                reverse=True
+            )
+            if not files:
+                raise FileNotFoundError("No bronze CSV files found.")
+            bronze_path = os.path.join(bronze_dir, files[0])
+        filename = os.path.basename(bronze_path)
+        data_date = filename.replace('bronze_', '').replace('.csv', '')
+        return clean_to_silver(bronze_path, data_date)
+
+    trigger_gold = TriggerDagRunOperator(
+        task_id='trigger_victoria_gold_dag',
+        trigger_dag_id='victoria_gold_dag',
+        execution_date='{{ execution_date }}'
+    )
+
+    run_clean_to_silver() >> trigger_gold
+
+victoria_silver_dag()
